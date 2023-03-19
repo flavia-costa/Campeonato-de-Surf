2e95bfd44ba0067b115410788158bb3c3e6a2875
Flavia Costa <109604714+flavia-costa@users.noreply.github.com>
2023-03-19 10:35:46 -0300

    Add files via upload

---
+++ campeonatoSurf.py
@@ -0,0 +1,46 @@
+def vencedor(nome, pontos):
+
+    vencedores = list()
+    maiores_pontos = list()
+    for x in range(3):
+        y = pontos.index(max(pontos))
+        maiores_pontos.append(pontos[y])
+        vencedores.append(nome[y])
+        nome.pop(y)
+        pontos.pop(y) 
+    
+    return vencedores, maiores_pontos
+
+a = 0
+b = 0
+
+print('Somente os 3 primeiros participantes serão mostrados.')
+print('-'*54)
+print('Sistema de Ranking - Campeonato de Surf 2023')
+print('-'*54)
+
+participantes = list()
+scores = list()
+pt = 0
+contador = 1
+
+while True:
+
+    participantes.append(str(input('Informe o nome do participante: ')))
+    while contador !=0:
+        pt = int(input('Informe a pontuação final do participante: '))
+        contador = scores.count(pt)
+        if contador == 0:
+            scores.append(pt)
+        else:
+            print('\nNão é permitido pelo sistema que sejam registradas duas pontuações iguais. Não há empates neste torneio.')
+    contador = 1
+    continua = str(input('\nDeseja realizar um novo cadastro? Digite (S/N).')).upper()
+    
+    if (continua!='S'):
+        break
+
+a, b = vencedor(participantes, scores)
+print('\nColocações: ')
+for x in range(3):
+    print(f'{x+1}º lugar: {a[x]} - {b[x]} Pontos')
